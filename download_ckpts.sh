fa4c7aeda2999ca264dcfd4f0de6f556b49ab524
Chen Wang <chwangthu@gmail.com>
2026-01-14 01:07:53 -0500

    Update pip install command for huggingface_hub

--- download_ckpts.sh
+++ download_ckpts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p checkpoints
-pip install -U "huggingface_hub[cli]"
+pip install -U "huggingface_hub[cli]<1.0"
 huggingface-cli download chenwang/physctrl --local-dir checkpoints --local-dir-use-symlinks False
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -O checkpoints/sam_vit_h_4b8939.pth
